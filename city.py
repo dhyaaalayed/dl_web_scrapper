0bb8e9fd6673b3ccf7c4b1db861eb0c603b0a532
Dieaa Al Aied <dhyaa.alayed@gmail.com>
2022-08-28 13:56:07 +0200

    Move web_main to a new file

--- city.py
+++ city.py
@@ -348,22 +348,6 @@ class NVT:
             })
 
 
-class LoadFromWebMain:
-    """
-        City class is only used, when we load the data for the first time, or when we want to overwrite the current json files
-    """
-    
-    cities = []
-    navigator = None
-
-    def __init__(self, root_paths):
-        self.navigator = Navigator()
-        for path in root_paths:
-            city = City("Dresden", path, self.navigator)
-
-            already_downloaded_list = ["1016", "1015", "1030", "1018", "1019", "1014", "1011", "1010", "1012"]
-            city.initialize_nvt_dict_using_web_navigator(already_downloaded_list = [])
-            self.cities.append(city)
 
 
 
@@ -401,9 +385,6 @@ if __name__ == "__main__":
           "/Users/dlprojectsit/Library/CloudStorage/OneDrive-SharedLibraries-DLProjectsGmbH/Data Management DL Projects - BAU/RV-07 Dresden/BVH-01 Dresden Cotta/Cotta West/Baupläne (HK+NVT)"
         , "/Users/dlprojectsit/Library/CloudStorage/OneDrive-SharedLibraries-DLProjectsGmbH/Data Management DL Projects - BAU/RV-07 Dresden/BVH-01 Dresden Cotta/Cotta Ost/Baupläne (HK+NVT)"
         ]
-    # 1- If we want to load from web
-    LoadFromWebMain(roots_list)
 
-    # 2- If we want to load from jsons
     # main = LoadFromJson(roots_list)
 
