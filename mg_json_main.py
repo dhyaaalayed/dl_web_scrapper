66853e5eaa84ea66e4a4464e97aebc34d8267dad
Dieaa Al Aied <dhyaa.alayed@gmail.com>
2022-09-25 02:09:19 +0200

    Update json service running time

--- mg_json_main.py
+++ mg_json_main.py
@@ -118,7 +118,7 @@ if __name__ == "__main__":
     while True:
         current_time = datetime.datetime.now()
         current_time_number = current_time.hour + current_time.minute/100
-        if 2.0 < current_time_number < 2.1:
+        if 0.1 < current_time_number < 0.2:
             main()
 
 
